a4ef1e44e0fa1a19d21cd9e9feadfaff1a63aafc
shinbunbun <takamuneyuto@gmail.com>
2022-04-18 23:02:47 +0900

    add actix-web

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+mod router;
+use actix_web::{App, HttpServer};
+// use dotenv::dotenv;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    // dotenv().ok();
+
+    HttpServer::new(move || App::new().configure(router::router))
+        .bind(("localhost", 8080))?
+        .run()
+        .await
 }
