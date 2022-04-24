4b82cbc418d61c5cc62a63eb2ef323b07cdc982e
shinbunbun <takamuneyuto@gmail.com>
2022-04-24 11:45:01 +0900

    add mod

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 mod bot;
+mod config;
+mod error;
+mod models;
 mod router;
 
 use actix_web::{App, HttpServer};
-// use dotenv::dotenv;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // dotenv().ok();
-
     HttpServer::new(move || App::new().configure(router::router))
         .bind(("localhost", 8080))?
         .run()
