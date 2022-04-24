19f8e03eb4069451683df877e3f5f5ea1822bfca
shinbunbun <takamuneyuto@gmail.com>
2022-04-24 11:45:29 +0900

    implement echo bot

--- src/bot/handler.rs
+++ src/bot/handler.rs
@@ -1,43 +1,66 @@
-use actix_web::{web, HttpResponse, Responder};
-use serde::{Deserialize, Serialize};
+use std::vec;
 
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Root {
-    pub destination: String,
-    pub events: Vec<Event>,
-}
+use crate::config;
+use crate::{error::AppError, models::webhook_event_object};
+use actix_web::{http::header, web, HttpResponse, Responder};
+use awc::Client;
+use serde::Serialize;
 
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Event {
-    #[serde(rename = "type")]
-    pub type_field: String,
-    pub message: Option<Message>,
-    pub timestamp: i64,
-    pub source: Source,
-    pub reply_token: Option<String>,
-    pub mode: String,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Message {
-    #[serde(rename = "type")]
-    pub type_field: String,
-    pub id: String,
-    pub text: String,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Source {
-    #[serde(rename = "type")]
-    pub type_field: String,
-    pub user_id: String,
-}
-
-pub async fn handler(context: web::Json<Root>) -> impl Responder {
+pub async fn handler(
+    context: web::Json<webhook_event_object::Root>,
+) -> Result<impl Responder, AppError> {
     println!("{:?}", context);
-    return HttpResponse::Ok().json("Ok");
+    for event in &context.events {
+        let message = event
+            .message
+            .as_ref()
+            .ok_or_else(|| AppError::BadRequest("Message not found".to_string()))?;
+        println!("{:?}", message.text);
+
+        let reply_messages = ReplyMessage {
+            reply_token: event
+                .reply_token
+                .as_ref()
+                .ok_or_else(|| AppError::BadRequest("Reply token not found".to_string()))?
+                .to_string(),
+            messages: vec![{
+                Message {
+                    r#type: "text".to_string(),
+                    text: message.text.to_string(),
+                }
+            }],
+        };
+
+        println!("{:?}", &reply_messages);
+
+        let mut response = Client::new()
+            .post("https://api.line.me/v2/bot/message/reply")
+            .insert_header((
+                header::AUTHORIZATION,
+                format!(
+                    "{}{}",
+                    "Bearer ".to_string(),
+                    config::get_token().map_err(AppError::Env)?
+                ),
+            ))
+            .send_json(&reply_messages)
+            .await
+            .map_err(AppError::AwcRequestError)?;
+
+        println!("{:?}", response.body().await.unwrap());
+    }
+    return Ok(HttpResponse::Ok().json("Ok"));
+}
+
+#[derive(Debug, Serialize)]
+struct ReplyMessage {
+    #[serde(rename(serialize = "replyToken"))]
+    reply_token: String,
+    messages: Vec<Message>,
+}
+
+#[derive(Debug, Serialize)]
+struct Message {
+    r#type: String,
+    text: String,
 }
