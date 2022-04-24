e8268fccd8dc5b61c990ffde561276c67726a85a
shinbunbun <takamuneyuto@gmail.com>
2022-04-24 11:44:57 +0900

    add config

---
+++ src/config.rs
@@ -0,0 +1,9 @@
+use std::env::{self, VarError};
+
+fn get_env(key: String) -> Result<String, VarError> {
+    env::var(key)
+}
+
+pub fn get_token() -> Result<String, VarError> {
+    get_env("CHANNEL_ACCESS_TOKEN".to_string())
+}
