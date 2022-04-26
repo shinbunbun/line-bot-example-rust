d5d4d216711e7837c00df754d07719e9f869466f
shinbunbun <takamuneyuto@gmail.com>
2022-04-26 23:41:46 +0900

    add quick reply object

---
+++ src/models/message/quick_reqly.rs
@@ -0,0 +1,17 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct QuickReply {
+    pub items: Vec<Item>,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Item {
+    #[serde(rename = "type")]
+    pub type_field: String,
+    pub image_url: Option<String>,
+    // TODO: add action object
+    // pub action: Action,
+}
