1eeb91493d111f5c97e3ab94b6830e979e20e971
shinbunbun <takamuneyuto@gmail.com>
2022-05-01 18:45:18 +0900

    add quick_reply

--- src/models/message/quick_reply.rs
+++ src/models/message/quick_reply.rs
@@ -1,3 +1,4 @@
+use crate::models::action::Actions;
 use serde::{Deserialize, Serialize};
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
@@ -6,12 +7,11 @@ pub struct QuickReply {
     pub items: Vec<Item>,
 }
 
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Item {
     #[serde(rename = "type")]
     pub type_field: String,
     pub image_url: Option<String>,
-    // TODO: add action object
-    // pub action: Action,
+    pub action: Actions,
 }
