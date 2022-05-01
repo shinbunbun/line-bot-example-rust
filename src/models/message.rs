ca8de51a24883aca1fa6b6ee239f307ec83da7f3
shinbunbun <takamuneyuto@gmail.com>
2022-05-01 19:01:22 +0900

    update message.rs

--- src/models/message.rs
+++ src/models/message.rs
@@ -1,10 +1,49 @@
-use self::quick_reqly::QuickReply;
+use self::{quick_reply::QuickReply, sender::Sender, stamp::StampMessage, text::TextMessage};
 use serde::{Deserialize, Serialize};
 
-mod quick_reqly;
+pub mod quick_reply;
+pub mod sender;
+pub mod stamp;
+pub mod text;
 
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct Message {
+pub struct MessageObject {
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub quick_reply: Option<QuickReply>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub sender: Option<Sender>,
+    #[serde(flatten)]
+    pub message: EachMessageFields,
 }
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum EachMessageFields {
+    TextMessage(TextMessage),
+    StampMessage(StampMessage),
+}
+
+/* fn test() {
+    let s = MessageObject {
+        quick_reply: None,
+        sender: None,
+        message: EachMessageFields::Text(Text {
+            text: "hello".to_string(),
+            type_field: "text".to_string(),
+            emojis: None,
+        }),
+    };
+    let json = serde_json::to_string(&s).unwrap();
+    println!("{}", json);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn internal() {
+        test();
+    }
+} */
