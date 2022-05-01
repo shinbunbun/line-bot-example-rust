1d48ac7c07710b57585e8aba56f7732404cae91b
shinbunbun <takamuneyuto@gmail.com>
2022-05-01 18:49:34 +0900

    add sender

---
+++ src/models/message/sender.rs
@@ -0,0 +1,8 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Sender {
+    pub name: String,
+    pub icon_url: String,
+}
