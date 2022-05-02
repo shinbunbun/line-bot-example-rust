8fab417a093bd2dd9063288161010e6d60bbc7a1
shinbunbun <takamuneyuto@gmail.com>
2022-05-02 13:16:38 +0900

    add verify signature middleware

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,18 @@ mod config;
 mod error;
 mod models;
 mod router;
+mod verify_signature;
 
 use actix_web::{App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(move || App::new().configure(router::router))
-        .bind(("localhost", 8080))?
-        .run()
-        .await
+    HttpServer::new(move || {
+        App::new()
+            .configure(router::router)
+            .wrap(verify_signature::Verifier)
+    })
+    .bind(("localhost", 8080))?
+    .run()
+    .await
 }
