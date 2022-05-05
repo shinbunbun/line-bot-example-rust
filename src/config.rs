a72b7ba144f9606fdbbada16fbc483c801fcc4bb
shinbunbun <takamuneyuto@gmail.com>
2022-05-05 16:18:02 +0900

    middlewareの実装をやめた

--- src/config.rs
+++ src/config.rs
@@ -7,3 +7,7 @@ fn get_env(key: String) -> Result<String, VarError> {
 pub fn get_token() -> Result<String, VarError> {
     get_env("CHANNEL_ACCESS_TOKEN".to_string())
 }
+
+pub fn get_secret() -> Result<String, VarError> {
+    get_env("CHANNEL_SECRET".to_string())
+}
--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,13 @@ mod config;
 mod error;
 mod models;
 mod router;
-mod verify_signature;
 
 use actix_web::{App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(move || {
-        App::new()
-            .configure(router::router)
-            .wrap(verify_signature::Verifier)
-    })
-    .bind(("localhost", 8080))?
-    .run()
-    .await
+    HttpServer::new(move || App::new().configure(router::router))
+        .bind(("localhost", 8080))?
+        .run()
+        .await
 }
