f65deff0f1e3e671393b07a4737b38fce5c9817a
shinbunbun <takamuneyuto@gmail.com>
2022-05-06 21:29:50 +0900

    refactor verify_signature

--- src/bot/handler.rs
+++ src/bot/handler.rs
@@ -1,8 +1,8 @@
+use crate::bot::verify_signature;
 use crate::client;
 use log::info;
 use std::vec;
 
-use crate::config;
 use crate::extractor::CustomHeader;
 use crate::models::message::text::TextMessage;
 use crate::models::message::EachMessageFields;
@@ -10,26 +10,16 @@ use crate::models::message::MessageObject;
 use crate::{error::AppError, models::webhook_event};
 use actix_web::{HttpResponse, Responder};
 
-use hmac::Hmac;
-use hmac::Mac;
 use serde::Serialize;
-use sha2::Sha256;
 
 pub async fn handler(
     context: String,
     custom_header: CustomHeader,
 ) -> Result<impl Responder, AppError> {
     info!("Request body: {}", context);
-    type HmacSha256 = Hmac<Sha256>;
-    let secret = config::get_secret().map_err(AppError::Env)?;
-    let mut mac =
-        HmacSha256::new_from_slice(secret.as_bytes()).map_err(AppError::HmacInvalidLength)?;
-    mac.update(context.as_bytes());
 
-    let x_line_signature =
-        base64::decode(custom_header.x_line_signature).map_err(AppError::Base64Decode)?;
-    mac.verify_slice(&x_line_signature[..])
-        .map_err(AppError::HmacVerifyError)?;
+    let x_line_signature = custom_header.x_line_signature;
+    verify_signature::verify(&x_line_signature, context.clone())?;
 
     let context: webhook_event::Root =
         serde_json::from_str(context.as_str()).map_err(AppError::SerdeJson)?;
@@ -59,9 +49,10 @@ pub async fn handler(
             }],
         };
 
-        let mut response =
+        let response =
             client::line_post_request(reply_messages, "https://api.line.me/v2/bot/message/reply")
                 .await?;
+        info!("API Response: {:#?}", response);
     }
     return Ok(HttpResponse::Ok().json("Ok"));
 }
--- src/bot/verify_signature.rs
+++ src/bot/verify_signature.rs
@@ -1,1 +1,17 @@
-fn verify_signature(signature: &str, body: &[u8]) {}
+use hmac::Hmac;
+use hmac::Mac;
+use sha2::Sha256;
+
+use crate::{config, error::AppError};
+
+pub fn verify(signature: &str, context: String) -> Result<(), AppError> {
+    type HmacSha256 = Hmac<Sha256>;
+    let secret = config::get_secret().map_err(AppError::Env)?;
+    let mut mac =
+        HmacSha256::new_from_slice(secret.as_bytes()).map_err(AppError::HmacInvalidLength)?;
+    mac.update(context.as_bytes());
+
+    let x_line_signature = base64::decode(signature).map_err(AppError::Base64Decode)?;
+    mac.verify_slice(&x_line_signature[..])
+        .map_err(AppError::HmacVerifyError)
+}
