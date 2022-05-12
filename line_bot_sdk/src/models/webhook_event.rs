059aa81ae824a66d92c17b7ffcff798eafcbfb03
shinbunbun <takamuneyuto@gmail.com>
2022-05-12 21:23:05 +0900

    add File

--- line_bot_sdk/src/models/webhook_event.rs
+++ line_bot_sdk/src/models/webhook_event.rs
@@ -1,10 +1,4 @@
-use std::{collections::HashMap, hash::Hash};
-
-use log::info;
-use serde::{
-    de::{self, value::MapAccessDeserializer, IntoDeserializer, VariantAccess, Visitor},
-    Deserialize,
-};
+use serde::Deserialize;
 
 #[derive(Default, Debug, Clone, PartialEq, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -41,80 +35,13 @@ pub enum Message {
     Video(Video),
     Image(Image),
     Audio(Audio),
+    File(File),
 }
-
-/* impl<'de> Deserialize<'de> for Message {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: serde::Deserializer<'de>,
-    {
-        struct FieldVisitor;
-        impl<'de> Visitor<'de> for FieldVisitor {
-            type Value = Message;
-
-            fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-                formatter.write_str("`text`, `video`, or `image`")
-            }
-
-            /* fn visit_str<E>(self, value: &str) -> Result<Self::Value, E>
-            where
-                E: serde::de::Error,
-            {
-                Deserialize::deserialize(value.into_deserializer()).map(Message::Text)
-            } */
-
-            fn visit_map<A>(self, mut map: A) -> Result<Self::Value, A::Error>
-            where
-                A: serde::de::MapAccess<'de>,
-            {
-                /* while let Some(x) = map.next_entry()? {
-                    println!("{:?}", x);
-                    return Deserialize::deserialize(MapAccessDeserializer::new(map))
-                        .map(Message::Text);
-                } */
-                /* while true {
-                    if(key.0 == "type") {
-                        match key.1 {
-                            "text" => {
-
-
-                        }
-                    }
-                } */
-                /* while let Some(x) = map.next_entry()? {
-                    println!("{:?}", x);
-                }; */
-                let mut message_map = HashMap::<String, String>::new();
-                let mut message_type: String;
-                while let Some((key, value)) = map.next_entry::<String, String>()? {
-                    println!("{}, {}", key, value);
-                    message_map.insert(key.clone(), value.clone());
-                    if key != "type" {
-                        continue;
-                    }
-                    match value.as_str() {
-                        "text" => {
-                            message_type = value;
-                        }
-                        _ => {
-                            return Err(serde::de::Error::custom("unexpected message type"));
-                        }
-                    }
-                }
-                println!("{:#?}", message_map);
-                Deserialize::deserialize(MapAccessDeserializer::new(map)).map(Message::Text)
-            }
-        }
-        deserializer.deserialize_any(FieldVisitor)
-    }
-} */
 
 #[derive(Debug, Clone, PartialEq, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Text {
     pub id: String,
-    // #[serde(rename = "type")]
-    // pub type_field: String,
     pub text: String,
     pub emojis: Option<Vec<Emoji>>,
     pub mention: Option<Mention>,
@@ -192,4 +119,12 @@ pub struct Audio {
     pub id: String,
     pub duration: i64,
     pub content_provider: ContentProvider,
+}
+
+#[derive(Debug, Clone, PartialEq, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct File {
+    pub id: String,
+    pub file_name: String,
+    pub file_size: i64,
 }
