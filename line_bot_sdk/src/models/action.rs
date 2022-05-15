c4d4c7c707731a83134c08261b91f29d177eacc4
shinbunbun <takamuneyuto@gmail.com>
2022-05-15 22:18:15 +0900

    fix Actions & add method

--- line_bot_sdk/src/models/action.rs
+++ line_bot_sdk/src/models/action.rs
@@ -2,15 +2,16 @@ use serde::{Deserialize, Serialize};
 // use validator::Validate;
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(untagged)]
 pub enum Actions {
-    PostbackAction,
-    MessageAction,
-    URIAction,
-    DatetimePickerAction,
-    CameraAction,
-    CameraRollAction,
-    LocationAction,
-    RichMenuSwitchAction,
+    PostbackAction(PostbackAction),
+    MessageAction(MessageAction),
+    URIAction(URIAction),
+    DatetimePickerAction(DatetimePickerAction),
+    CameraAction(CameraAction),
+    CameraRollAction(CameraRollAction),
+    LocationAction(LocationAction),
+    RichMenuSwitchAction(RichMenuSwitchAction),
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize /* , Validate */)]
@@ -18,16 +19,51 @@ pub enum Actions {
 pub struct PostbackAction {
     #[serde(rename = "type")]
     pub type_field: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub label: Option<String>,
     // #[validate(length(max = 300))]
     pub data: String,
     // #[validate(length(max = 300))]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub display_text: Option<String>,
 
     /// This property will be abolished in the future.
     /// https://developers.line.biz/en/reference/messaging-api/#postback-action
     // #[validate(length(max = 300))]
-    pub text: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub text: Option<String>,
+    pub input_option: Option<String>,
+    pub fill_in_text: Option<String>,
+}
+
+impl PostbackAction {
+    pub fn new(label: String, data: String) -> Self {
+        Self {
+            type_field: "postback".to_string(),
+            label: Some(label),
+            data,
+            display_text: None,
+            text: None,
+            input_option: None,
+            fill_in_text: None,
+        }
+    }
+    pub fn with_display_text(mut self, display_text: String) -> Self {
+        self.display_text = Some(display_text);
+        self
+    }
+    pub fn with_text(mut self, text: String) -> Self {
+        self.text = Some(text);
+        self
+    }
+    pub fn with_input_option(mut self, input_option: String) -> Self {
+        self.input_option = Some(input_option);
+        self
+    }
+    pub fn with_fill_in_text(mut self, fill_in_text: String) -> Self {
+        self.fill_in_text = Some(fill_in_text);
+        self
+    }
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize /* , Validate */)]
@@ -77,12 +113,29 @@ pub struct CameraAction {
     pub label: String,
 }
 
+impl CameraAction {
+    pub fn new(label: String) -> Self {
+        Self {
+            type_field: "camera".to_string(),
+            label,
+        }
+    }
+}
+
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct CameraRollAction {
     #[serde(rename = "type")]
     pub type_field: String,
     pub label: String,
+}
+impl CameraRollAction {
+    pub fn new(label: String) -> Self {
+        Self {
+            type_field: "cameraRoll".to_string(),
+            label,
+        }
+    }
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
@@ -91,6 +144,14 @@ pub struct LocationAction {
     #[serde(rename = "type")]
     pub type_field: String,
     pub label: String,
+}
+impl LocationAction {
+    pub fn new(label: String) -> Self {
+        Self {
+            type_field: "location".to_string(),
+            label,
+        }
+    }
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
