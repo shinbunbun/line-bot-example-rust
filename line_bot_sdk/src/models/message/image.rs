d5c2b48965a9b4d48e0dcbf888290f3f21609b94
shinbunbun <takamuneyuto@gmail.com>
2022-05-15 23:07:40 +0900

    impl ImageMessage constructor

--- line_bot_sdk/src/models/message/image.rs
+++ line_bot_sdk/src/models/message/image.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 
 use super::{quick_reply::QuickReply, sender::Sender, CommonFields};
 
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct ImageMessage {
     #[serde(rename = "type")]
@@ -23,5 +23,17 @@ impl CommonFields for ImageMessage {
     fn with_sender(mut self, sender: super::sender::Sender) -> Self {
         self.sender = Some(sender);
         self
+    }
+}
+
+impl ImageMessage {
+    pub fn new(original_content_url: String, preview_image_url: String) -> Self {
+        ImageMessage {
+            type_field: "image".to_string(),
+            original_content_url,
+            preview_image_url,
+            quick_reply: None,
+            sender: None,
+        }
     }
 }
