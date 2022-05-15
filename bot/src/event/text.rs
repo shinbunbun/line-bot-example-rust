7b209d33ce32acb8c42d3201c8ca13653330daaa
shinbunbun <takamuneyuto@gmail.com>
2022-05-15 22:20:04 +0900

    add "クイックリプライ"

--- bot/src/event/text.rs
+++ bot/src/event/text.rs
@@ -1,6 +1,10 @@
 use line_bot_sdk::{
     error::AppError,
-    models::{message::text::TextMessage, message::MessageObject, webhook_event::Text},
+    models::{
+        message::{quick_reply::QuickReply, MessageObject},
+        message::{text::TextMessage, CommonFields, quick_reply::Item},
+        webhook_event::Text, action::{Actions, PostbackAction, CameraAction, CameraRollAction, LocationAction},
+    },
 };
 
 pub fn text_event(message: &Text) -> Result<Vec<MessageObject>, AppError> {
@@ -12,6 +16,13 @@ pub fn text_event(message: &Text) -> Result<Vec<MessageObject>, AppError> {
         "複数メッセージ" => vec![
             MessageObject::Text(TextMessage::new("Hello, user".to_string())),
             MessageObject::Text(TextMessage::new("May I help you?".to_string())),
+        ],
+        "クイックリプライ" => vec![
+            MessageObject::Text(TextMessage::new("クイックリプライ（以下のアクションはクイックリプライ専用で、他のメッセージタイプでは使用できません）".to_string()).with_quick_reply(QuickReply{ items: vec![
+                Item::new(Actions::CameraAction(CameraAction::new("カメラを開く".to_string()))),
+                Item::new(Actions::CameraRollAction(CameraRollAction::new("カメラロールを開く".to_string()))), 
+                Item::new(Actions::LocationAction(LocationAction::new("位置情報画面を開く".to_string()))),
+            ]})) 
         ],
         _ => vec![{
             MessageObject::Text(TextMessage::new(format!(
