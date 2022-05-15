84ed65cc28e8b60dd8edbd86e57c5839e0372b20
shinbunbun <takamuneyuto@gmail.com>
2022-05-15 23:03:20 +0900

    impl CommonFields for TemplateMessage

--- line_bot_sdk/src/models/message/template.rs
+++ line_bot_sdk/src/models/message/template.rs
@@ -5,6 +5,8 @@ use self::{
     image_carousel::ImageCarouselTemplate,
 };
 
+use super::{quick_reply::QuickReply, sender::Sender, CommonFields};
+
 pub mod buttons;
 pub mod carousel;
 pub mod confirm;
@@ -17,6 +19,21 @@ pub struct TemplateMessage {
     pub type_field: String,
     pub alt_text: String,
     pub template: Template,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub quick_reply: Option<QuickReply>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub sender: Option<Sender>,
+}
+
+impl CommonFields for TemplateMessage {
+    fn with_quick_reply(mut self, quick_reply: super::quick_reply::QuickReply) -> Self {
+        self.quick_reply = Some(quick_reply);
+        self
+    }
+    fn with_sender(mut self, sender: super::sender::Sender) -> Self {
+        self.sender = Some(sender);
+        self
+    }
 }
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
