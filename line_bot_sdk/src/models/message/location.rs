33beca8c05e033acc2e9976a30869d590cf588af
shinbunbun <takamuneyuto@gmail.com>
2022-05-15 22:58:20 +0900

    impl CommonFields for LocationMessage

--- line_bot_sdk/src/models/message/location.rs
+++ line_bot_sdk/src/models/message/location.rs
@@ -1,5 +1,7 @@
 use serde::{Deserialize, Serialize};
 
+use super::{quick_reply::QuickReply, sender::Sender, CommonFields};
+
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct LocationMessage {
@@ -9,4 +11,19 @@ pub struct LocationMessage {
     pub address: String,
     pub latitude: f64,
     pub longitude: f64,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub quick_reply: Option<QuickReply>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub sender: Option<Sender>,
+}
+
+impl CommonFields for LocationMessage {
+    fn with_quick_reply(mut self, quick_reply: super::quick_reply::QuickReply) -> Self {
+        self.quick_reply = Some(quick_reply);
+        self
+    }
+    fn with_sender(mut self, sender: super::sender::Sender) -> Self {
+        self.sender = Some(sender);
+        self
+    }
 }
