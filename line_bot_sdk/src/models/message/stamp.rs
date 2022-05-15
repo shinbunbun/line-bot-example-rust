8f01f8c06bdf3ec86e960147988f7ba3e89913c7
shinbunbun <takamuneyuto@gmail.com>
2022-05-15 23:02:32 +0900

    impl CommonFields for StampMessage

--- line_bot_sdk/src/models/message/stamp.rs
+++ line_bot_sdk/src/models/message/stamp.rs
@@ -1,5 +1,7 @@
 use serde::{Deserialize, Serialize};
 
+use super::{quick_reply::QuickReply, sender::Sender, CommonFields};
+
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct StampMessage {
@@ -7,4 +9,19 @@ pub struct StampMessage {
     pub type_field: String,
     pub package_id: String,
     pub sticker_id: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub quick_reply: Option<QuickReply>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub sender: Option<Sender>,
+}
+
+impl CommonFields for StampMessage {
+    fn with_quick_reply(mut self, quick_reply: super::quick_reply::QuickReply) -> Self {
+        self.quick_reply = Some(quick_reply);
+        self
+    }
+    fn with_sender(mut self, sender: super::sender::Sender) -> Self {
+        self.sender = Some(sender);
+        self
+    }
 }
