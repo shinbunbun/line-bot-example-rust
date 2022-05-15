a3ba629fde37857aee282cb7a4233010057dae95
shinbunbun <takamuneyuto@gmail.com>
2022-05-15 22:50:34 +0900

    impl CommonFields for ImagemapMessage

--- line_bot_sdk/src/models/message/imagemap.rs
+++ line_bot_sdk/src/models/message/imagemap.rs
@@ -1,5 +1,7 @@
 use serde::{Deserialize, Serialize};
 
+use super::{quick_reply::QuickReply, sender::Sender, CommonFields};
+
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct ImagemapMessage {
@@ -9,6 +11,21 @@ pub struct ImagemapMessage {
     pub alt_text: String,
     pub base_size: BaseSize,
     pub video: Option<Video>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub quick_reply: Option<QuickReply>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub sender: Option<Sender>,
+}
+
+impl CommonFields for ImagemapMessage {
+    fn with_quick_reply(mut self, quick_reply: super::quick_reply::QuickReply) -> Self {
+        self.quick_reply = Some(quick_reply);
+        self
+    }
+    fn with_sender(mut self, sender: super::sender::Sender) -> Self {
+        self.sender = Some(sender);
+        self
+    }
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
