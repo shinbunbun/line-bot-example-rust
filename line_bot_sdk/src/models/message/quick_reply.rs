9f3a7b06f294f483a7528527d37f01446bbf09e1
shinbunbun <takamuneyuto@gmail.com>
2022-05-15 22:17:25 +0900

    add Item method

--- line_bot_sdk/src/models/message/quick_reply.rs
+++ line_bot_sdk/src/models/message/quick_reply.rs
@@ -12,6 +12,21 @@ pub struct QuickReply {
 pub struct Item {
     #[serde(rename = "type")]
     pub type_field: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub image_url: Option<String>,
     pub action: Actions,
+}
+
+impl Item {
+    pub fn new(action: Actions) -> Self {
+        Self {
+            type_field: "action".to_string(),
+            image_url: None,
+            action,
+        }
+    }
+    pub fn with_image_url(mut self, image_url: String) -> Self {
+        self.image_url = Some(image_url);
+        self
+    }
 }
