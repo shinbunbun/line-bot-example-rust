c8c19751a7551e5ca962a815006d2a406e3b5e09
shinbunbun <takamuneyuto@gmail.com>
2022-05-17 14:18:40 +0900

    impl CarouselTemplate

--- line_bot_sdk/src/models/message/template/carousel.rs
+++ line_bot_sdk/src/models/message/template/carousel.rs
@@ -12,6 +12,25 @@ pub struct CarouselTemplate {
     pub image_size: Option<String>,
 }
 
+impl CarouselTemplate {
+    pub fn new(columns: Vec<Column>) -> CarouselTemplate {
+        CarouselTemplate {
+            type_field: "carousel".to_string(),
+            columns,
+            image_aspect_ratio: None,
+            image_size: None,
+        }
+    }
+    pub fn with_image_aspect_ratio(mut self, image_aspect_ratio: String) -> Self {
+        self.image_aspect_ratio = Some(image_aspect_ratio);
+        self
+    }
+    pub fn with_image_size(mut self, image_size: String) -> Self {
+        self.image_size = Some(image_size);
+        self
+    }
+}
+
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Column {
