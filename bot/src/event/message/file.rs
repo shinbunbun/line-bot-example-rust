75864b5310d5b5dd589dc794d133f07401d7a24a
shinbunbun <takamuneyuto@gmail.com>
2022-05-26 21:09:05 +0900

    fix file

--- bot/src/event/message/file.rs
+++ bot/src/event/message/file.rs
@@ -4,8 +4,11 @@ use line_bot_sdk::{
 };
 
 pub fn handler(message: &File) -> Result<Option<Vec<MessageObject>>, AppError> {
-    println!("{:?}", message);
-    Ok(Some(vec![MessageObject::Text(TextMessage::new(
+    /* Ok(Some(vec![MessageObject::Text(TextMessage::new(
         "ファイルを受け取りました！".to_string(),
-    ))]))
+    ))])) */
+    Ok(Some(vec![TextMessage::builder()
+        .text("ファイルを受け取りました！")
+        .build()
+        .into()]))
 }
