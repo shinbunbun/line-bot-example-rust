6cad8a8fcd1e4946cf2054a14250285b34f6023b
shinbunbun <takamuneyuto@gmail.com>
2022-05-26 18:20:57 +0900

    add TypedBuilder for buttons

--- line_bot_sdk/src/models/message/template/buttons.rs
+++ line_bot_sdk/src/models/message/template/buttons.rs
@@ -1,35 +1,53 @@
 use serde::{Deserialize, Serialize};
+use typed_builder::TypedBuilder;
 
 use crate::models::{
     action::Actions,
     message::{quick_reply::QuickReply, sender::Sender},
 };
 
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+use super::Template;
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, TypedBuilder)]
 #[serde(rename_all = "camelCase")]
 pub struct ButtonsTemplate {
     #[serde(rename = "type")]
+    #[builder(default = "buttons".to_string())]
     pub type_field: String,
     #[serde(skip_serializing_if = "Option::is_none")]
+    #[builder(default, setter(transform = |x: &str| Some(x.to_string())))]
     pub thumbnail_image_url: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
+    #[builder(default, setter(transform = |x: &str| Some(x.to_string())))]
     pub image_aspect_ratio: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
+    #[builder(default, setter(transform = |x: &str| Some(x.to_string())))]
     pub image_size: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
+    #[builder(default, setter(transform = |x: &str| Some(x.to_string())))]
     pub image_background_color: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
+    #[builder(default, setter(transform = |x: &str| Some(x.to_string())))]
     pub title: Option<String>,
+    #[builder(setter(transform = |x: &str| x.to_string()))]
     pub text: String,
     pub default_action: Actions,
     pub actions: Vec<Actions>,
     #[serde(skip_serializing_if = "Option::is_none")]
+    #[builder(default, setter(strip_option))]
     pub quick_reply: Option<QuickReply>,
     #[serde(skip_serializing_if = "Option::is_none")]
+    #[builder(default, setter(strip_option))]
     pub sender: Option<Sender>,
 }
 
-impl ButtonsTemplate {
+impl From<ButtonsTemplate> for Template {
+    fn from(message: ButtonsTemplate) -> Self {
+        Template::Buttons(message)
+    }
+}
+
+/* impl ButtonsTemplate {
     pub fn new(text: String, default_action: Actions, actions: Vec<Actions>) -> Self {
         ButtonsTemplate {
             type_field: "buttons".to_string(),
@@ -45,24 +63,4 @@ impl ButtonsTemplate {
             sender: None,
         }
     }
-    pub fn with_thumbnail_image_url(mut self, thumbnail_image_url: String) -> Self {
-        self.thumbnail_image_url = Some(thumbnail_image_url);
-        self
-    }
-    pub fn with_image_aspect_ratio(mut self, image_aspect_ratio: String) -> Self {
-        self.image_aspect_ratio = Some(image_aspect_ratio);
-        self
-    }
-    pub fn with_image_size(mut self, image_size: String) -> Self {
-        self.image_size = Some(image_size);
-        self
-    }
-    pub fn with_image_background_color(mut self, image_background_color: String) -> Self {
-        self.image_background_color = Some(image_background_color);
-        self
-    }
-    pub fn with_title(mut self, title: String) -> Self {
-        self.title = Some(title);
-        self
-    }
-}
+} */
