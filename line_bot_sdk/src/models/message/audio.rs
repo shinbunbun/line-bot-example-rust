7b9e1cccf5cff1bd4ca56f8961e58d1614492946
shinbunbun <takamuneyuto@gmail.com>
2022-05-26 16:45:28 +0900

    add TypedBuilder for AudioMessage

--- line_bot_sdk/src/models/message/audio.rs
+++ line_bot_sdk/src/models/message/audio.rs
@@ -1,20 +1,32 @@
 use serde::{Deserialize, Serialize};
+use typed_builder::TypedBuilder;
 
-use super::{quick_reply::QuickReply, sender::Sender, Message};
+use super::{quick_reply::QuickReply, sender::Sender, Message, MessageObject};
 
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, TypedBuilder)]
 #[serde(rename_all = "camelCase")]
 pub struct AudioMessage {
     #[serde(rename = "type")]
+    #[builder(default = "audio".to_string())]
     pub type_field: String,
+    #[builder(setter(transform = |x: &str| x.to_string()))]
     pub original_content_url: String,
     pub duration: u64,
     #[serde(skip_serializing_if = "Option::is_none")]
+    #[builder(default, setter(strip_option))]
     pub quick_reply: Option<QuickReply>,
     #[serde(skip_serializing_if = "Option::is_none")]
+    #[builder(default, setter(strip_option))]
     pub sender: Option<Sender>,
 }
 
+impl From<AudioMessage> for MessageObject {
+    fn from(message: AudioMessage) -> Self {
+        MessageObject::Audio(message)
+    }
+}
+
+/*
 impl Message<'_> for AudioMessage {
     fn with_quick_reply(mut self, quick_reply: super::quick_reply::QuickReply) -> Self {
         self.quick_reply = Some(quick_reply);
@@ -40,3 +52,4 @@ impl AudioMessage {
         super::MessageObject::Audio(self)
     }
 }
+ */
