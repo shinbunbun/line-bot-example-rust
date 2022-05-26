f9a48fdd3a91715d4886d5223ab2464cc9d64ec3
shinbunbun <takamuneyuto@gmail.com>
2022-05-26 21:08:05 +0900

    fix location

--- bot/src/event/message/location.rs
+++ bot/src/event/message/location.rs
@@ -4,9 +4,12 @@ use line_bot_sdk::{
 };
 
 pub fn handler(message: &Location) -> Result<Option<Vec<MessageObject>>, AppError> {
-    println!("{:?}", message);
-    Ok(Some(vec![MessageObject::Text(TextMessage::new(format!(
+    /* Ok(Some(vec![MessageObject::Text(TextMessage::new(format!(
         "受け取った住所: {}",
         message.address
-    )))]))
+    )))])) */
+    Ok(Some(vec![TextMessage::builder()
+        .text(&format!("受け取った住所: {}", message.address))
+        .build()
+        .into()]))
 }
