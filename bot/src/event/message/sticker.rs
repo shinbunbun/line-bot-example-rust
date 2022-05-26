45c5706b1d15b593ac14733c44106fda75bed1df
shinbunbun <takamuneyuto@gmail.com>
2022-05-26 21:08:15 +0900

    fix sticker

--- bot/src/event/message/sticker.rs
+++ bot/src/event/message/sticker.rs
@@ -9,14 +9,26 @@ use line_bot_sdk::{
 
 pub fn handler(message: &Sticker) -> Result<Option<Vec<MessageObject>>, AppError> {
     if message.sticker_id == "1988" {
-        Ok(Some(vec![MessageObject::Stamp(StampMessage::new(
+        /* Ok(Some(vec![MessageObject::Stamp(StampMessage::new(
             "446".to_string(),
             "1989".to_string(),
-        ))]))
+        ))])) */
+        Ok(Some(vec![StampMessage::builder()
+            .package_id("446")
+            .sticker_id("1989")
+            .build()
+            .into()]))
     } else {
-        Ok(Some(vec![MessageObject::Text(TextMessage::new(format!(
+        /* Ok(Some(vec![MessageObject::Text(TextMessage::new(format!(
             "受け取ったstickerId: {}\nそのスタンプの返信には対応してません...",
             message.sticker_id
-        )))]))
+        )))])) */
+        Ok(Some(vec![TextMessage::builder()
+            .text(&format!(
+                "受け取ったstickerId: {}\nそのスタンプの返信には対応してません...",
+                message.sticker_id
+            ))
+            .build()
+            .into()]))
     }
 }
