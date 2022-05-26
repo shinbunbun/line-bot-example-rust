8cb422aa201b4c3a8d8afd17a8656a363184992f
shinbunbun <takamuneyuto@gmail.com>
2022-05-26 21:45:47 +0900

    delete comment

--- line_bot_sdk/src/models/message/image.rs
+++ line_bot_sdk/src/models/message/image.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 use typed_builder::TypedBuilder;
 
-use super::{quick_reply::QuickReply, sender::Sender, Message, MessageObject};
+use super::{quick_reply::QuickReply, sender::Sender, MessageObject};
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize, TypedBuilder)]
 #[serde(rename_all = "camelCase")]
@@ -26,29 +26,3 @@ impl From<ImageMessage> for MessageObject {
         MessageObject::Image(message)
     }
 }
-
-/* impl Message<'_> for ImageMessage {
-    fn with_quick_reply(mut self, quick_reply: super::quick_reply::QuickReply) -> Self {
-        self.quick_reply = Some(quick_reply);
-        self
-    }
-    fn with_sender(mut self, sender: super::sender::Sender) -> Self {
-        self.sender = Some(sender);
-        self
-    }
-}
-
-impl ImageMessage {
-    pub fn new(original_content_url: String, preview_image_url: String) -> Self {
-        ImageMessage {
-            type_field: "image".to_string(),
-            original_content_url,
-            preview_image_url,
-            quick_reply: None,
-            sender: None,
-        }
-    }
-    pub fn build(self) -> super::MessageObject {
-        super::MessageObject::Image(self)
-    }
-} */
