10d49e660b4275ad65c5391b8a8488be4c2c3aed
shinbunbun <takamuneyuto@gmail.com>
2022-05-26 16:45:12 +0900

    add TypedBuilderfor ImageMessage

--- line_bot_sdk/src/models/message/image.rs
+++ line_bot_sdk/src/models/message/image.rs
@@ -1,21 +1,33 @@
 use serde::{Deserialize, Serialize};
+use typed_builder::TypedBuilder;
 
-use super::{quick_reply::QuickReply, sender::Sender, Message};
+use super::{quick_reply::QuickReply, sender::Sender, Message, MessageObject};
 
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, TypedBuilder)]
 #[serde(rename_all = "camelCase")]
 pub struct ImageMessage {
     #[serde(rename = "type")]
+    #[builder(default = "image".to_string())]
     pub type_field: String,
+    #[builder(setter(transform = |x: &str| x.to_string()))]
     pub original_content_url: String,
+    #[builder(setter(transform = |x: &str| x.to_string()))]
     pub preview_image_url: String,
     #[serde(skip_serializing_if = "Option::is_none")]
+    #[builder(default, setter(strip_option))]
     pub quick_reply: Option<QuickReply>,
     #[serde(skip_serializing_if = "Option::is_none")]
+    #[builder(default, setter(strip_option))]
     pub sender: Option<Sender>,
 }
 
-impl Message<'_> for ImageMessage {
+impl From<ImageMessage> for MessageObject {
+    fn from(message: ImageMessage) -> Self {
+        MessageObject::Image(message)
+    }
+}
+
+/* impl Message<'_> for ImageMessage {
     fn with_quick_reply(mut self, quick_reply: super::quick_reply::QuickReply) -> Self {
         self.quick_reply = Some(quick_reply);
         self
@@ -39,4 +51,4 @@ impl ImageMessage {
     pub fn build(self) -> super::MessageObject {
         super::MessageObject::Image(self)
     }
-}
+} */
