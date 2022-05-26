9f8f0a9f3a0b432def653eb936f630ad9efd428e
shinbunbun <takamuneyuto@gmail.com>
2022-05-26 12:29:43 +0900

    add TypedBuilder for stamp

--- line_bot_sdk/src/models/message/stamp.rs
+++ line_bot_sdk/src/models/message/stamp.rs
@@ -1,21 +1,33 @@
 use serde::{Deserialize, Serialize};
+use typed_builder::TypedBuilder;
 
-use super::{quick_reply::QuickReply, sender::Sender, Message};
+use super::{quick_reply::QuickReply, sender::Sender, Message, MessageObject};
 
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, TypedBuilder)]
 #[serde(rename_all = "camelCase")]
 pub struct StampMessage {
     #[serde(rename = "type")]
+    #[builder(default = "sticker".to_string())]
     pub type_field: String,
+    #[builder(setter(transform = |x: &str| x.to_string()))]
     pub package_id: String,
+    #[builder(setter(transform = |x: &str| x.to_string()))]
     pub sticker_id: String,
     #[serde(skip_serializing_if = "Option::is_none")]
+    #[builder(default, setter(strip_option))]
     pub quick_reply: Option<QuickReply>,
     #[serde(skip_serializing_if = "Option::is_none")]
+    #[builder(default, setter(strip_option))]
     pub sender: Option<Sender>,
 }
 
-impl Message<'_> for StampMessage {
+impl From<StampMessage> for MessageObject {
+    fn from(message: StampMessage) -> Self {
+        MessageObject::Stamp(message)
+    }
+}
+
+/* impl Message<'_> for StampMessage {
     fn with_quick_reply(mut self, quick_reply: super::quick_reply::QuickReply) -> Self {
         self.quick_reply = Some(quick_reply);
         self
@@ -39,4 +51,4 @@ impl StampMessage {
     pub fn build(self) -> super::MessageObject {
         super::MessageObject::Stamp(self)
     }
-}
+} */
