ce136e34c1773e4ca94699e4d5241736614d0fca
shinbunbun <takamuneyuto@gmail.com>
2022-05-26 21:34:29 +0900

    fix audio

--- bot/src/event/message/audio.rs
+++ bot/src/event/message/audio.rs
@@ -4,11 +4,11 @@ use line_bot_sdk::{
 };
 
 pub fn handler(message: &Audio) -> Result<Option<Vec<MessageObject>>, AppError> {
-    /* Ok(Some(vec![MessageObject::Text(TextMessage::new(
-        "音声を受け取りました！".to_string(),
-    ))])) */
     Ok(Some(vec![TextMessage::builder()
-        .text("音声を受け取りました！")
+        .text(&format!(
+            "音声を受け取りました！\nメッセージID: {}",
+            message.id
+        ))
         .build()
         .into()]))
 }
