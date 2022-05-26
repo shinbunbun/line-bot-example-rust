cf4eb0caefeb036d34771856943cf1c881b0cb11
shinbunbun <takamuneyuto@gmail.com>
2022-05-26 18:28:50 +0900

    add TypedBuilder for confirm

--- line_bot_sdk/src/models/message/template/confirm.rs
+++ line_bot_sdk/src/models/message/template/confirm.rs
@@ -1,24 +1,37 @@
 use serde::{Deserialize, Serialize};
+use typed_builder::TypedBuilder;
 
 use crate::models::{
     action::Actions,
     message::{quick_reply::QuickReply, sender::Sender},
 };
 
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+use super::Template;
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, TypedBuilder)]
 #[serde(rename_all = "camelCase")]
 pub struct ConfirmTemplate {
     #[serde(rename = "type")]
+    #[builder(default = "confirm".to_string())]
     pub type_field: String,
+    #[builder(setter(transform = |x: &str| x.to_string()))]
     pub text: String,
     pub actions: Vec<Actions>,
     #[serde(skip_serializing_if = "Option::is_none")]
+    #[builder(default, setter(strip_option))]
     pub quick_reply: Option<QuickReply>,
     #[serde(skip_serializing_if = "Option::is_none")]
+    #[builder(default, setter(strip_option))]
     pub sender: Option<Sender>,
 }
 
-impl ConfirmTemplate {
+impl From<ConfirmTemplate> for Template {
+    fn from(message: ConfirmTemplate) -> Self {
+        Template::Confirm(message)
+    }
+}
+
+/* impl ConfirmTemplate {
     pub fn new(text: String, actions: Vec<Actions>) -> Self {
         ConfirmTemplate {
             type_field: "confirm".to_string(),
@@ -28,4 +41,4 @@ impl ConfirmTemplate {
             sender: None,
         }
     }
-}
+} */
