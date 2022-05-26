9b6c6d6f7e8b556c70aa47d29b804d8f3d8b2333
shinbunbun <takamuneyuto@gmail.com>
2022-05-26 11:02:23 +0900

    add TypedBuilder for QuickReply

--- line_bot_sdk/src/models/message/quick_reply.rs
+++ line_bot_sdk/src/models/message/quick_reply.rs
@@ -1,29 +1,32 @@
 use crate::models::action::Actions;
 use serde::{Deserialize, Serialize};
+use typed_builder::TypedBuilder;
 
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, TypedBuilder)]
 #[serde(rename_all = "camelCase")]
 pub struct QuickReply {
-    pub items: Vec<Item>,
+    pub items: Vec<QuickReplyItem>,
 }
 
-impl QuickReply {
-    pub fn new(items: Vec<Item>) -> Self {
+/* impl QuickReply {
+    pub fn new(items: Vec<QuickReplyItem>) -> Self {
         Self { items }
     }
-}
+} */
 
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, TypedBuilder)]
 #[serde(rename_all = "camelCase")]
-pub struct Item {
+pub struct QuickReplyItem {
     #[serde(rename = "type")]
+    #[builder(default = "action")]
     pub type_field: String,
     #[serde(skip_serializing_if = "Option::is_none")]
+    #[builder(default, setter(strip_option))]
     pub image_url: Option<String>,
     pub action: Actions,
 }
 
-impl Item {
+/* impl QuickReplyItem {
     pub fn new(image_url: Option<String>, action: Actions) -> Self {
         Self {
             type_field: "action".to_string(),
@@ -31,4 +34,4 @@ impl Item {
             action,
         }
     }
-}
+} */
