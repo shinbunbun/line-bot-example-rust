1ba987c976856a0048b7e404c712d5184923ce9d
shinbunbun <takamuneyuto@gmail.com>
2022-05-26 20:42:14 +0900

    add TypedBuilder for carousel

--- line_bot_sdk/src/models/message/template/carousel.rs
+++ line_bot_sdk/src/models/message/template/carousel.rs
@@ -1,25 +1,41 @@
 use serde::{Deserialize, Serialize};
+use typed_builder::TypedBuilder;
 
 use crate::models::{
     action::Actions,
     message::{quick_reply::QuickReply, sender::Sender},
 };
 
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+use super::Template;
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, TypedBuilder)]
 #[serde(rename_all = "camelCase")]
 pub struct CarouselTemplate {
     #[serde(rename = "type")]
+    #[builder(default = "carousel".to_string())]
     pub type_field: String,
     pub columns: Vec<Column>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[builder(default, setter(strip_option))]
     pub image_aspect_ratio: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[builder(default, setter(strip_option))]
     pub image_size: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
+    #[builder(default, setter(strip_option))]
     pub quick_reply: Option<QuickReply>,
     #[serde(skip_serializing_if = "Option::is_none")]
+    #[builder(default, setter(strip_option))]
     pub sender: Option<Sender>,
 }
 
-impl CarouselTemplate {
+impl From<CarouselTemplate> for Template {
+    fn from(template: CarouselTemplate) -> Self {
+        Template::Carousel(template)
+    }
+}
+
+/* impl CarouselTemplate {
     pub fn new(columns: Vec<Column>) -> CarouselTemplate {
         CarouselTemplate {
             type_field: "carousel".to_string(),
@@ -38,20 +54,29 @@ impl CarouselTemplate {
         self.image_size = Some(image_size);
         self
     }
-}
+} */
 
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, TypedBuilder)]
 #[serde(rename_all = "camelCase")]
 pub struct Column {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[builder(default, setter(transform = |x: &str| Some(x.to_string())))]
     pub thumbnail_image_url: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[builder(default, setter(transform = |x: &str| Some(x.to_string())))]
     pub image_background_color: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[builder(default, setter(transform = |x: &str| Some(x.to_string())))]
     pub title: Option<String>,
+    #[builder(setter(transform = |x: &str| x.to_string()))]
     pub text: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[builder(default, setter(strip_option))]
     pub default_action: Option<Actions>,
     pub actions: Vec<Actions>,
 }
 
-impl Column {
+/* impl Column {
     pub fn new(text: String, actions: Vec<Actions>) -> Column {
         Column {
             thumbnail_image_url: None,
@@ -78,4 +103,4 @@ impl Column {
         self.default_action = Some(default_action);
         self
     }
-}
+} */
