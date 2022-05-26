4668dfe20eb4219c4df17a09e38707c570e46812
shinbunbun <takamuneyuto@gmail.com>
2022-05-26 21:08:31 +0900

    fix follow

--- bot/src/event/follow.rs
+++ bot/src/event/follow.rs
@@ -4,7 +4,11 @@ use line_bot_sdk::{
 };
 
 pub async fn index() -> Result<Option<Vec<MessageObject>>, AppError> {
-    Ok(Some(vec![MessageObject::Text(TextMessage::new(
+    /* Ok(Some(vec![MessageObject::Text(TextMessage::new(
         "友達追加ありがとうございます！".to_string(),
-    ))]))
+    ))])) */
+    Ok(Some(vec![TextMessage::builder()
+        .text("友達追加ありがとうございます！")
+        .build()
+        .into()]))
 }
