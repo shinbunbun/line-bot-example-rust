ea4473d9f5efad7a41047a900fbdb3eea73b3605
shinbunbun <takamuneyuto@gmail.com>
2022-05-26 16:45:41 +0900

    add TypedBuilder for VideoMessage

--- line_bot_sdk/src/models/message/video.rs
+++ line_bot_sdk/src/models/message/video.rs
@@ -1,23 +1,36 @@
 use serde::{Deserialize, Serialize};
+use typed_builder::TypedBuilder;
 
-use super::{quick_reply::QuickReply, sender::Sender, Message};
+use super::{quick_reply::QuickReply, sender::Sender, Message, MessageObject};
 
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, TypedBuilder)]
 #[serde(rename_all = "camelCase")]
 pub struct VideoMessage {
     #[serde(rename = "type")]
+    #[builder(default = "video".to_string())]
     pub type_field: String,
+    #[builder(setter(transform = |x: &str| x.to_string()))]
     pub original_content_url: String,
+    #[builder(setter(transform = |x: &str| x.to_string()))]
     pub preview_image_url: String,
     #[serde(skip_serializing_if = "Option::is_none")]
+    #[builder(default, setter(transform = |x: Option<String>| x.map(|x| x.to_string())))]
     pub tracking_id: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
+    #[builder(default, setter(strip_option))]
     pub quick_reply: Option<QuickReply>,
     #[serde(skip_serializing_if = "Option::is_none")]
+    #[builder(default, setter(strip_option))]
     pub sender: Option<Sender>,
 }
 
-impl Message<'_> for VideoMessage {
+impl From<VideoMessage> for MessageObject {
+    fn from(message: VideoMessage) -> Self {
+        MessageObject::Video(message)
+    }
+}
+
+/* impl Message<'_> for VideoMessage {
     fn with_quick_reply(mut self, quick_reply: super::quick_reply::QuickReply) -> Self {
         self.quick_reply = Some(quick_reply);
         self
@@ -43,4 +56,7 @@ impl VideoMessage {
         self.tracking_id = Some(tracking_id);
         self
     }
-}
+    pub fn build(self) -> super::MessageObject {
+        super::MessageObject::Video(self)
+    }
+} */
