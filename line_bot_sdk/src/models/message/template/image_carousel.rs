7392e67dd51bae919831d9bc0d25a8f20eb320bb
shinbunbun <takamuneyuto@gmail.com>
2022-05-26 20:42:41 +0900

    add TypedBuilder for image crousel

--- line_bot_sdk/src/models/message/template/image_carousel.rs
+++ line_bot_sdk/src/models/message/template/image_carousel.rs
@@ -1,23 +1,35 @@
 use serde::{Deserialize, Serialize};
+use typed_builder::TypedBuilder;
 
 use crate::models::{
     action::Actions,
     message::{quick_reply::QuickReply, sender::Sender},
 };
 
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+use super::Template;
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, TypedBuilder)]
 #[serde(rename_all = "camelCase")]
 pub struct ImageCarouselTemplate {
     #[serde(rename = "type")]
+    #[builder(default = "image_carousel".to_string())]
     pub type_field: String,
     pub columns: Vec<Column>,
     #[serde(skip_serializing_if = "Option::is_none")]
+    #[builder(default, setter(strip_option))]
     pub quick_reply: Option<QuickReply>,
     #[serde(skip_serializing_if = "Option::is_none")]
+    #[builder(default, setter(strip_option))]
     pub sender: Option<Sender>,
 }
 
-impl ImageCarouselTemplate {
+impl From<ImageCarouselTemplate> for Template {
+    fn from(template: ImageCarouselTemplate) -> Self {
+        Template::ImageCarousel(template)
+    }
+}
+
+/* impl ImageCarouselTemplate {
     pub fn new(columns: Vec<Column>) -> ImageCarouselTemplate {
         ImageCarouselTemplate {
             type_field: "image_carousel".to_string(),
@@ -26,17 +38,18 @@ impl ImageCarouselTemplate {
             sender: None,
         }
     }
-}
+} */
 
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, TypedBuilder)]
 #[serde(rename_all = "camelCase")]
 pub struct Column {
+    #[builder(setter(transform = |x: &str| x.to_string()))]
     pub image_url: String,
     pub action: Actions,
 }
 
-impl Column {
+/* impl Column {
     pub fn new(image_url: String, action: Actions) -> Column {
         Column { image_url, action }
     }
-}
+} */
