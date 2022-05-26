5c4a062e526afed5414b4dedd883120e8134be9a
shinbunbun <takamuneyuto@gmail.com>
2022-05-26 16:45:59 +0900

    add TypedBuilder for LocationMessage

--- line_bot_sdk/src/models/message/location.rs
+++ line_bot_sdk/src/models/message/location.rs
@@ -1,23 +1,35 @@
 use serde::{Deserialize, Serialize};
+use typed_builder::TypedBuilder;
 
-use super::{quick_reply::QuickReply, sender::Sender, Message};
+use super::{quick_reply::QuickReply, sender::Sender, Message, MessageObject};
 
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize, TypedBuilder)]
 #[serde(rename_all = "camelCase")]
 pub struct LocationMessage {
     #[serde(rename = "type")]
+    #[builder(default = "location".to_string())]
     pub type_field: String,
+    #[builder(setter(transform = |x: &str| x.to_string()))]
     pub title: String,
+    #[builder(setter(transform = |x: &str| x.to_string()))]
     pub address: String,
     pub latitude: f64,
     pub longitude: f64,
     #[serde(skip_serializing_if = "Option::is_none")]
+    #[builder(default, setter(strip_option))]
     pub quick_reply: Option<QuickReply>,
     #[serde(skip_serializing_if = "Option::is_none")]
+    #[builder(default, setter(strip_option))]
     pub sender: Option<Sender>,
 }
 
-impl Message<'_> for LocationMessage {
+impl From<LocationMessage> for MessageObject {
+    fn from(message: LocationMessage) -> Self {
+        MessageObject::Location(message)
+    }
+}
+
+/* impl Message<'_> for LocationMessage {
     fn with_quick_reply(mut self, quick_reply: super::quick_reply::QuickReply) -> Self {
         self.quick_reply = Some(quick_reply);
         self
@@ -43,4 +55,4 @@ impl LocationMessage {
     pub fn build(self) -> super::MessageObject {
         super::MessageObject::Location(self)
     }
-}
+} */
