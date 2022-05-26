e81bf520911b3d0619a471974648a7724546b62e
shinbunbun <takamuneyuto@gmail.com>
2022-05-26 12:28:19 +0900

    fix text TypedBuilder

--- line_bot_sdk/src/models/message/text.rs
+++ line_bot_sdk/src/models/message/text.rs
@@ -9,7 +9,7 @@ pub struct TextMessage {
     #[serde(rename = "type")]
     #[builder(default = "text".to_string())]
     pub type_field: String,
-    #[builder(setter(transform = |text: &str| text.to_string()))]
+    #[builder(setter(transform = |x: &str| x.to_string()))]
     pub text: String,
     #[serde(skip_serializing_if = "Option::is_none")]
     #[builder(default, setter(strip_option))]
@@ -62,8 +62,8 @@ impl From<TextMessage> for MessageObject {
 #[serde(rename_all = "camelCase")]
 pub struct Emoji {
     pub index: u32,
-    #[builder(setter(transform = |text: &str| text.to_string()))]
+    #[builder(setter(transform = |x: &str| x.to_string()))]
     pub product_id: String,
-    #[builder(setter(transform = |text: &str| text.to_string()))]
+    #[builder(setter(transform = |x: &str| x.to_string()))]
     pub emoji_id: String,
 }
