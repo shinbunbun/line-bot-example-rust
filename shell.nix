ef0f5c1d859f25403cee3111f0ce7a0d4acaf851
Endered <yy56ga10ve@gmail.com>
2022-05-31 23:52:29 +0900

    restore shell.nix

--- shell.nix
+++ shell.nix
@@ -15,6 +15,7 @@ in pkgs.mkShell {
     rust-analyzer
   ];
 
+  #
   # Certain Rust tools won't work without this
   # This can also be fixed by using oxalica/rust-overlay and specifying the rust-src extension
   # See https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570/3?u=samuela. for more details.
