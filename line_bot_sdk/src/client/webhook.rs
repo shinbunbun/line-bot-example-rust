a0e4997e10327f19182ffacf46748b1c4ba8e75a
shinbunbun <takamuneyuto@gmail.com>
2022-07-07 17:00:02 +0900

    webhook設定のtestを追加

--- line_bot_sdk/src/client/webhook.rs
+++ line_bot_sdk/src/client/webhook.rs
@@ -34,7 +34,7 @@ impl Client {
 
     pub async fn test_webhook_endpoint_url(
         &self,
-        endpoint: Option<String>,
+        endpoint: Option<&str>,
     ) -> Result<TestWebhookEndpointUrlResponse, Error> {
         let mut res = match endpoint {
             Some(endpoint) => {
@@ -48,7 +48,7 @@ impl Client {
             }
             None => {
                 self.post(
-                    (),
+                    Empty {},
                     &format!("{}/v2/bot/channel/webhook/endpoint/test", API_ENDPOINT_BASE),
                 )
                 .await?
@@ -81,4 +81,53 @@ pub struct TestWebhookEndpointUrlResponse {
     pub status_code: i16,
     pub reason: String,
     pub detail: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct Empty {}
+
+#[cfg(test)]
+mod test {
+    use std::env;
+
+    use chrono::Local;
+
+    use crate::Client;
+
+    fn get_client() -> crate::Client {
+        crate::Client::new(
+            env::var("CHANNEL_ACCESS_TOKEN").unwrap(),
+            env::var("CHANNEL_SECRET").unwrap(),
+        )
+    }
+
+    #[actix_web::test]
+    async fn test_webhook_endpoint_url() {
+        let client = get_client();
+        let endpoint = &format!("https://example.com/{}", Local::now().timestamp());
+        test_set_webhook_event_url(&client, endpoint).await;
+        test_get_webhook_endpoint_info(&client, endpoint).await;
+        // test_test_webhook_endpoint_url(&client).await;
+        // test_test_webhook_endpoint_url_with_endpoint(&client, endpoint).await;
+    }
+
+    async fn test_set_webhook_event_url(client: &Client, endpoint: &str) {
+        client.set_webhook_event_url(endpoint).await.unwrap();
+    }
+
+    async fn test_get_webhook_endpoint_info(client: &Client, endpoint: &str) {
+        let info = client.get_webhook_endpoint_info().await.unwrap();
+        assert_eq!(info.endpoint, endpoint);
+    }
+
+    /* async fn test_test_webhook_endpoint_url(client: &Client) {
+        client.test_webhook_endpoint_url(None).await.unwrap();
+    }
+
+    async fn test_test_webhook_endpoint_url_with_endpoint(client: &Client, endpoint: &str) {
+        client
+            .test_webhook_endpoint_url(Some(endpoint))
+            .await
+            .unwrap();
+    } */
 }
