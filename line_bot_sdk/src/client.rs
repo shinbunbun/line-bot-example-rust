1036c5391ab069aef9a3db467dc79e94c22ee1b3
shinbunbun <takamuneyuto@gmail.com>
2022-07-08 11:03:47 +0900

    clientにpost_formを実装

--- line_bot_sdk/src/client.rs
+++ line_bot_sdk/src/client.rs
@@ -93,6 +93,25 @@ impl Client {
         Ok(response)
     }
 
+    async fn post_form<T: serde::Serialize>(
+        &self,
+        body: T,
+        url: &str,
+    ) -> Result<ClientResponse<Decoder<Payload>>, Error> {
+        let json = serde_json::to_string(&body).expect("json encode error");
+        let mut response = awc::Client::new()
+            .post(url)
+            .send_form(&body)
+            .await
+            .map_err(Error::AwcSendRequestError)?;
+        if response.status() != 200 {
+            let res_body = response.body().await.map_err(Error::ActixWebPayloadError)?;
+            let res_body = String::from_utf8(res_body.to_vec()).map_err(Error::FromUtf8Error)?;
+            return Err(Error::AWCClientError(res_body, json));
+        }
+        Ok(response)
+    }
+
     #[allow(clippy::all)]
     pub async fn put<T: serde::Serialize>(
         &self,
