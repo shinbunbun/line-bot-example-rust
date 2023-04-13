07c8866ff5029e1ff078af1132e23e004fe03372
shinbunbun <takamuneyuto@gmail.com>
2023-04-13 15:47:21 +0900

    add richmenu&richmenu_validate

---
+++ line_bot_sdk/src/client/richmenu.rs
@@ -0,0 +1,58 @@
+use serde::{Deserialize, Serialize};
+use typed_builder::TypedBuilder;
+
+use crate::{awc_wrapper::SendClientRequestFut, models::action::Actions, Client};
+
+use super::API_ENDPOINT_BASE;
+
+#[derive(Debug, Clone, Serialize, Deserialize, TypedBuilder)]
+pub struct RichMenuObject {
+    pub size: RichMenuSize,
+    pub selected: bool,
+    pub name: String,
+    pub chat_bar_text: String,
+    pub areas: Vec<RichMenuArea>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, TypedBuilder)]
+pub struct RichMenuSize {
+    pub width: u32,
+    pub height: u32,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, TypedBuilder)]
+pub struct RichMenuArea {
+    pub bounds: RichMenuBounds,
+    pub action: Actions,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, TypedBuilder)]
+pub struct RichMenuBounds {
+    pub x: u32,
+    pub y: u32,
+    pub width: u32,
+    pub height: u32,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct RichMenuResponse {
+    pub rich_menu_id: String,
+}
+
+impl Client {
+    pub fn richmenu(&self, richmenu: RichMenuObject) -> SendClientRequestFut<RichMenuResponse> {
+        SendClientRequestFut::new(self.post(
+            richmenu,
+            &format!("{}/v2/bot/richmenu", API_ENDPOINT_BASE),
+            None,
+        ))
+    }
+
+    pub fn richmenu_validate(&self, richmenu: RichMenuObject) -> SendClientRequestFut<()> {
+        SendClientRequestFut::new(self.post(
+            richmenu,
+            &format!("{}/v2/bot/richmenu/validate", API_ENDPOINT_BASE),
+            None,
+        ))
+    }
+}
