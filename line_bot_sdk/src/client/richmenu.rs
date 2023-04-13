98014ed62a687f74ab93a384baf070cb492423d3
shinbunbun <takamuneyuto@gmail.com>
2023-04-13 20:54:09 +0900

    impl iroiro

--- line_bot_sdk/src/client/richmenu.rs
+++ line_bot_sdk/src/client/richmenu.rs
@@ -51,12 +51,18 @@ pub struct PostRichMenuResponse {
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct RichMenuResponseObject {
-    pub richmenu_id: String,
+    pub rich_menu_id: String,
     pub size: RichMenuSize,
     pub selected: bool,
     pub name: String,
     pub chat_bar_text: String,
     pub areas: Vec<RichMenuArea>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct GetUserAllRichMenuResponse {
+    pub rich_menu_id: String,
 }
 
 impl Client {
@@ -115,13 +121,44 @@ impl Client {
         ))
     }
 
-    pub fn user_all_richmenu(&self, richmenu_id: &str) -> SendClientRequestFut<()> {
+    pub fn post_user_all_richmenu(&self, richmenu_id: &str) -> SendClientRequestFut<()> {
         SendClientRequestFut::new(self.post(
             (),
             &format!(
                 "{}/v2/bot/user/all/richmenu/{}",
                 API_ENDPOINT_BASE, richmenu_id
             ),
+            None,
+        ))
+    }
+
+    pub fn get_user_all_richmenu(&self) -> SendClientRequestFut<GetUserAllRichMenuResponse> {
+        SendClientRequestFut::new(self.get(
+            &format!("{}/v2/bot/user/all/richmenu", API_ENDPOINT_BASE),
+            None::<&[(); 0]>,
+            None,
+            true,
+        ))
+    }
+
+    pub fn delete_user_all_richmenu(&self) -> SendClientRequestFut<()> {
+        SendClientRequestFut::new(self.delete(
+            &format!("{}/v2/bot/user/all/richmenu", API_ENDPOINT_BASE),
+            None::<&[(); 0]>,
+        ))
+    }
+
+    pub fn post_richmenu_alias(
+        &self,
+        rich_menu_alias_id: &str,
+        rich_menu_id: &str,
+    ) -> SendClientRequestFut<()> {
+        SendClientRequestFut::new(self.post(
+            &[
+                ("richMenuId", rich_menu_id),
+                ("richMenuAliasId", rich_menu_alias_id),
+            ],
+            &format!("{}/v2/bot/richmenu/alias/", API_ENDPOINT_BASE),
             None,
         ))
     }
