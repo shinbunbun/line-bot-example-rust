1fd48adca87fd6d1ad95708f2e67c2f4508084e9
shinbunbun <takamuneyuto@gmail.com>
2023-04-17 10:45:27 +0900

    impl save audio

--- src/event/message.rs
+++ src/event/message.rs
@@ -27,7 +27,7 @@ pub async fn index(
         Message::Text(text_message) => text::text_event(app_context, event, text_message).await,
         Message::Image(image_message) => image::handler(image_message),
         Message::Video(video_message) => video::handler(video_message),
-        Message::Audio(audio_message) => audio::handler(audio_message),
+        Message::Audio(audio_message) => audio::handler(app_context, audio_message).await,
         Message::File(file_message) => file::handler(file_message),
         Message::Location(location_message) => location::handler(location_message),
         Message::Sticker(sticker_message) => sticker::handler(sticker_message),
